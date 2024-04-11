f8bd3d30435a478d164ea3b5c80592941c4d2cf0
ggome1 <bookincar@naver.com>
2024-04-11 20:42:17 +0900

    chore: 초기 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 const MyUtils = require("../Utils/MyUtils");
 
 class App {
-  async play() {}
+  async play() {
+    MyUtils.Console.print("버전을 입력해주세요 (숫자 버전: 1, 영어 버전: 2) : ");
+    const version = MyUtils.Console.readLineAsync();
+    switch(version) {
+      case '1':
+        caseNumber();
+        break;
+      case '2':
+        caseAlpha();
+        break;
+      default:
+        throw Error("[ERROR] 존재하지 않는 버전입니다.");
+    }
+  }
+    
+  async caseNumber() {
+    MyUtils.Console.print("숫자를 입력해주세요(1 ~ 100) : ");      
+  }
+
+  async caseAlpha() {
+    MyUtils.Console.print("영어를 입력해주세요(A ~ z) : " );
+  }
 }
 
 module.exports = App;
