b674c5262bbca21e75c83f4c7b4bc2943ba20ac3
One-HyeWon <5876675@naver.com>
2024-04-18 04:51:09 +0900

    feat: 사용자 입력 함수 작성 (버전 입력)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 const MyUtils = require("../Utils/MyUtils");
 
 class App {
-  async play() {}
+  async play() {
+    MyUtils.Console.print("업다운 게임을 시작합니다.\n");
+    MyUtils.Console.print(
+      "버전을 입력해주세요 (숫자 버전: 1, 영어 버전: 2) : "
+    );
+    try {
+      const version = await this.chooseVersion();
+      const answer = this.generateAnswer(version);
+    } catch (error) {
+      throw new Error(`[ERROR] ${error.message}`);
+    }
+  }
+
+  async chooseVersion() {
+    const version = await MyUtils.Console.readLineAsync();
+    if (version !== "1" && version !== "2") {
+      throw new Error("존재하지 않는 버전입니다.");
+    }
+    return version;
+  }
 }
 
 module.exports = App;
